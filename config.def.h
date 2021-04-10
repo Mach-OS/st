319cda7dca42f5fd2b83d086081336ae03715270
christianchiarulli <chrisatmachine@gmail.com>
2021-04-10 15:47:50 -0400

    remove comments

--- config.def.h
+++ config.def.h
@@ -245,19 +245,6 @@ static MouseShortcut mshortcuts[] = {
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
 	{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
 	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
-
-	/* { XK_ANY_MOD,           Button2, clippaste,      {.i = 0},      1 }, */
-	/* { XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} }, */
-	/* { ShiftMask,            Button4, kscrollup,      {.i = 1} }, */
-	/*  	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} }, */
-	/* { ShiftMask,            Button5, kscrolldown,    {.i = 1} }, */
-
-	/* { ShiftMask,            Button4, kscrollup,      {.i = 1} }, */
-	/* { ShiftMask,            Button5, kscrolldown,    {.i = 1} }, */
-	/*  	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 }, */
-	/*  	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} }, */
-	/*  	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} }, */
-
 };
 
 /* Internal keyboard shortcuts. */
